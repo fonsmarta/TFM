2185144d3f200180b0b01267c8eff61152c0acf9
cocademolletes <155056908+cocademolletes@users.noreply.github.com>
2024-06-17 09:11:30 +0200

    Update crud.py
    Añadido el get_variants, que devuelve la lista completa de variantes

--- webservice/variants_app/crud.py
+++ webservice/variants_app/crud.py
@@ -2,14 +2,17 @@ from sqlalchemy.orm import Session
 
 from . import models, schemas
 
+def get_variants(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.Variante).offset(skip).limit(limit).all()
+
 def get_variant(db: Session, id: int):
-    return db.query(models.Variant).filter(models.Variant.id == id).first()
+    return db.query(models.Variante).filter(models.Variante.id == id).first()
 
 def get_variant_by_nombre(db: Session, nombre: str):
-    return db.query(models.Variant).filter(models.Variant.nombre == nombre).first()
+    return db.query(models.Variante).filter(models.Variante.nombre == nombre).first()
 
 def get_gen(db: Session, id_gen: int):
-    return db.query(models.Genes).filter(models.Genees.id_gen == idgen).first()
+    return db.query(models.Gen).filter(models.Gen.id_gen == idgen).first()
 
 def get_gen_by_nombre(db: Session, nombre: str):
-    return db.query(models.Genes).filter(models.Genes.nombre == nombre).first()
+    return db.query(models.Gen).filter(models.Gen.nombre == nombre).first()
