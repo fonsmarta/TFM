95b239406199fa1c7715f0f7e31ed4af8cb10666
cocademolletes <155056908+cocademolletes@users.noreply.github.com>
2024-06-17 09:13:10 +0200

    Update main.py
    Implementados dos enpoints: el que devuelve una variante si se le pasa el ID y el que devuelve una lista de todas las variantes.

--- webservice/variants_app/main.py
+++ webservice/variants_app/main.py
@@ -14,17 +14,22 @@ def get_db():
         yield db
     finally:
         db.close()
+		
+@app.get("/variants/", response_model=list[schemas.Variante])
+def read_variants(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    variants = crud.get_variants(db, skip=skip, limit=limit)
+    return variants
 
-@app.get("/variant/{id}", response_model=schemas.Variants)
+@app.get("/variant/{id}", response_model=schemas.Variante)
 def read_variants(id: int, db: Session = Depends(get_db)):
     db_var = crud.get_variant(db, id=id)
     if db_var is None:
         raise HTTPException(status_code=404, detail="Variant not found")
     return db_var
 
-@app.get("/variant/{name}", response_model=schemas.Variants)
-def read_variants_by_name(name: String, db: Session = Depends(get_db)):
-    db_var_name = crud.get_variant_by_namedb, name=name)
-    if db_var_name is None:
-        raise HTTPException(status_code=404, detail="Variant not found")
-    return db_var_name
+#@app.get("/variant/{name}", response_model=schemas.Variant)
+#def read_variants_by_name(nombre: String, db: Session = Depends(get_db)):
+#    db_var_name = crud.get_variant_by_nombre(db, nombre=nombre)
+#    if db_var_name is None:
+#        raise HTTPException(status_code=404, detail="Variant not found")
+#    return db_var_name
